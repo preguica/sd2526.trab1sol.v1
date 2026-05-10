a415ef71a75a4486f668fec43c06bd38d57ee38f
Tomas Silva <tomas.poim14@gmail.com>
2026-05-10 18:38:00 +0100

    initial tls server adapt

--- src/sd2526/trab/impl/rest/servers/AbstractRestServer.java
+++ src/sd2526/trab/impl/rest/servers/AbstractRestServer.java
@@ -1,6 +1,7 @@
 package sd2526.trab.impl.rest.servers;
 
 import java.net.URI;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
@@ -13,11 +14,11 @@ import sd2526.trab.impl.utils.IP;
 
 
 public abstract class AbstractRestServer extends AbstractServer {
-	private static final String SERVER_BASE_URI = "http://%s:%s%s";
+	private static final String SERVER_BASE_URI = "https://%s:%s%s";
 	private static final String REST_CTX = "/rest";
 
 	protected AbstractRestServer(Logger log, String service, int port) {
-		super(log, service, String.format(SERVER_BASE_URI, IP.hostAddress(), port, REST_CTX));
+		super(log, service, String.format(SERVER_BASE_URI, IP.hostname(), port, REST_CTX));
 	}
 
 	protected void start() {
@@ -25,8 +26,12 @@ public abstract class AbstractRestServer extends AbstractServer {
 		ResourceConfig config = new ResourceConfig();
 		
 		registerResources( config );
-		
-		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostAddress(), INETADDR_ANY)), config);
+
+		try {
+			JdkHttpServerFactory.createHttpServer(URI.create(String.format(SERVER_BASE_URI, INETADDR_ANY, URI.create(serverURI).getPort(), REST_CTX)), config, javax.net.ssl.SSLContext.getDefault());
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException("Failed to load default SSL context", e);
+		}
 		
 		if( service != null )
 			Discovery.getInstance().announce(serviceName(), super.serverURI);
